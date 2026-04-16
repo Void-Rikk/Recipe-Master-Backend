d7b893446c2c128167aceb08cd9676d62b126796
Denis <buddy.den@mail.ru>
2026-04-16 21:16:21 +0300

    database schema init

---
+++ src/db/schema.sql
@@ -0,0 +1,50 @@
+CREATE TABLE users
+(
+    id         SERIAL PRIMARY KEY,
+    first_name VARCHAR(50)  NOT NULL,
+    last_name  VARCHAR(50)  NOT NULL,
+    bio        TEXT,
+    password   VARCHAR(255) NOT NULL,
+    avatar_id  VARCHAR(36) UNIQUE
+);
+
+CREATE TABLE recipes
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(200) NOT NULL,
+    description TEXT         NOT NULL,
+    image_id    VARCHAR(36) UNIQUE,
+    user_id     INT          NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE ingredients
+(
+    id          SERIAL PRIMARY KEY,
+    description TEXT NOT NULL,
+    recipe_id   INT  NOT NULL REFERENCES recipes (id) ON DELETE CASCADE
+);
+
+CREATE TABLE instructions
+(
+    id            SERIAL PRIMARY KEY,
+    description   TEXT NOT NULL,
+    display_order INT DEFAULT 1,
+    recipe_id     INT  NOT NULL REFERENCES recipes (id) ON DELETE CASCADE
+);
+
+CREATE TABLE comments
+(
+    id        SERIAL PRIMARY KEY,
+    content   TEXT NOT NULL,
+    user_id   INT  NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    recipe_id INT  NOT NULL REFERENCES recipes (id) on DELETE CASCADE
+);
+
+CREATE TABLE likes
+(
+    user_id   INT NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    recipe_id INT NOT NULL REFERENCES recipes (id) ON DELETE CASCADE,
+
+    PRIMARY KEY (user_id, recipe_id)
+);
