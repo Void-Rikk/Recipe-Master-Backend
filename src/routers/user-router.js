888d3587d22f0444376921a80facb1bd8a6f18b2
Denis <buddy.den@mail.ru>
2026-05-06 14:42:04 +0300

    user router fixed to classic rest

--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -6,6 +6,6 @@ import { userAvatars } from "../storage/storage.js";
 const userRouter = new Router();
 
 userRouter.get("/user/:userId", UserController.getUserInfo);
-userRouter.patch("/updateUser/:userId", userAvatars.single("image"), UserController.updateUser);
+userRouter.patch("/user/:userId", userAvatars.single("image"), UserController.updateUser);
 
 export default userRouter;
