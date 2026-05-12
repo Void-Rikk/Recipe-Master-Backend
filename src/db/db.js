c01d0bcfb8135253ad6a405e01279b552e6eab93
Denis <buddy.den@mail.ru>
2026-05-12 10:59:57 +0300

    db config fix

--- src/db/db.js
+++ src/db/db.js
@@ -4,7 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = new Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export { db };
